e8da35e2da1e1de5f92b59b90c5b1a549bf613f3
Leonardo Dillon <jeannotegui@gmail.com>
2025-08-12 11:21:32 -0300

    prueba

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,18 @@
 import { defineConfig, envField } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    integrations: [mdx()],
+        
     env: {
         schema: {
             RESEND_API_TOKEN: envField.string( { context: 'server', access: 'secret' } ),
             MI_GMAIL : envField.string( { context: 'server', access: 'secret' } )
         }
     },
+    adapter: vercel(),
 
-    adapter: vercel()
 });
